84ca2224eeb55416b45730aac305051a369b9ae0
squarie <yarekinya@gmail.com>
2025-06-03 21:37:24 +0300

    ProfileController

--- RecipeWebsiteFrontend/src/pages/profile/profile_main.js
+++ RecipeWebsiteFrontend/src/pages/profile/profile_main.js
@@ -1,36 +1,13 @@
-// import { SUPABASE_URL, SUPABASE_KEY } from "/src/shared/js/config.js";
-// const { createClient } = supabase;
-// const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
-
-// async function getUser(id) {
-//     try {
-
-//       const { data } = await supabaseClient.public
-//         .from('users')
-//         .select('name, username, useremail') 
-//         .eq('id', id)
-//     }
-//     catch (e) {
-//         console.error(e)
-//     }
-//     if (data) {
-//         console.log(`name: ${data.name}, user: ${data.username}`);
-//       }
-    
-//       return data;
-// }
-
 async function test1(){
     document.write("hello")
 }
 test1()
 
-
 async function UsersData() {
     let token = localStorage.getItem("access_token");
 
     try {
-        let response = await fetch("http://localhost:5232/api/Users", {
+        let response = await fetch("http://localhost:5232/api/Profile", {
             headers: {
                 "Content-Type": "application/json",
                 Authorization: "Bearer " + token,
@@ -43,13 +20,13 @@ async function UsersData() {
 
         let data = await response.json();
         console.log(data);
-        let userName = document.createElement("div");
-        let userNickName = document.createElement("div");
+        let userName2 = document.createElement("div");
+        let userNickName2 = document.createElement("div");
 
         data.usersName = users.name; 
         data.usersNickName = users.username; 
-        userName.appendChild(data.usersName);
-        userNickName.appendChild(data.usersNickName);
+        userName2.appendChild(data.usersName);
+        userNickName2.appendChild(data.usersNickName);
         
     } catch (err) {
         console.error("Ошибка при получении пользователей:", err);
