47f0b3ed530ddc4fcdef1457a20b61dd2e681237
theun1c <belovdima1122@yandex.ru>
2025-06-04 14:01:03 +0300

    feat: added exit select (needed to fix)

---
+++ RecipeWebsiteFrontend/exitProfile.js
@@ -0,0 +1,10 @@
+// document.getElementById('exit-select').addEventListener('click', function () {
+//     if(this.value === 'logout'){
+//         logout();
+//     }
+// })
+
+// function logout(){
+//     window.location.href = "/index.html";
+//     localStorage.clear();
+// }
